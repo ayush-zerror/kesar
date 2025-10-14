1959a4483cc4dc713bff694b404767937e1357f4
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-10-14 14:53:17 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import Footer from "@/components/common/Footer";
+import Navbar from "@/components/common/Navbar";
+import SmoothScroller from "@/components/smoothScroll/SmoothScroller";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <SmoothScroller />
+    <Navbar />
+    <Component {...pageProps} />
+    <Footer />
+  </>;
 }
