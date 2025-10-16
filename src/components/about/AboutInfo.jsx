b5a1d30c5ea9ffb85e75e887c929115b3630d62b
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-10-16 17:23:46 +0530

    added info section

--- src/components/about/AboutInfo.jsx
+++ src/components/about/AboutInfo.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const AboutInfo = () => {
   return (
-    <div id='about_info'>
-        <div id="about_info_container"></div>
+    <div id="about_info">
+      <div id="about_info_container">
+        <div className="about_info_card">
+          <Image
+            width={1000}
+            height={1000}
+            src="/images/about/logo1.png"
+            alt="logo"
+          />
+          <h5>Manufacturing Units</h5>
+          <p>CPC crude · Alpha Blue · Beta Blue · Pigment Green 7</p>
+        </div>
+        <div className="about_info_card">
+          <Image
+            width={1000}
+            height={1000}
+            src="/images/about/logo2.png"
+            alt="logo"
+          />
+          <h5>Certifications</h5>
+          <p>ISO 9001:2015; REACH-registered grades</p>
+        </div>
+        <div className="about_info_card">
+          <Image
+            width={1000}
+            height={1000}
+            src="/images/about/logo3.png"
+            alt="logo"
+          />
+          <h5>Employees</h5>
+          <p>550+ employees, strong repeat business culture</p>
+        </div>
+        <div className="about_info_card">
+          <Image
+            width={1000}
+            height={1000}
+            src="/images/about/logo4.png"
+            alt="logo"
+          />
+          <h5>Headquarters</h5>
+          <p>BKC Mumbai, Maharashtra, India</p>
+        </div>
+        <div className="about_info_card">
+          <Image
+            width={1000}
+            height={1000}
+            src="/images/about/logo5.png"
+            alt="logo"
+          />
+          <h5>Manufacturing Plant</h5>
+          <p>MIDC Lote Parshuram, Ratnagiri, Maharashtra, India</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutInfo
+export default AboutInfo;
