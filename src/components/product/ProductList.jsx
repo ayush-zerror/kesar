e57c679aa0c65e4dd43b17df4c4ac431140209d5
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-10-17 14:11:38 +0530

    fixed build

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 
-const ProductList = ({ categories ,currentSlug  }) => {
+const ProductList = ({ categories, currentSlug }) => {
   return (
     <>
-      {categories.map((categoryItem, index) => (
+      {categories?.map((categoryItem, index) => (
         <div className="product_list" key={index}>
           <h5>{categoryItem?.category}</h5>
           <div className="product_list_container">
             {categoryItem?.products?.map((product, idx) => (
-              <ProductCard key={idx} product={product} currentSlug={currentSlug } />
+              <ProductCard
+                key={idx}
+                product={product}
+                currentSlug={currentSlug || ""} 
+              />
             ))}
           </div>
         </div>
